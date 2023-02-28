4159c70ea6f99733dc40a539b84e635d52a056ca
jessicashong <jessicashong12@gmail.com>
2023-02-27 18:07:24 -0600

    added to thought model--still need to add email validator on user model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
     {
@@ -10,7 +11,28 @@ const thoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            
-        }
-    }
-)
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    },
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
